1d8255f1b4376ee3bdc91b650813bd08b47dccc1
Tova Jertfelt <tovizitan@gmail.com>
2022-08-25 09:40:33 +0200

    blogs och fetch

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,34 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList"
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    
-  ])
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id); 
     setBlogs(newBlogs);
   }
 
+  //?går ej att använda async och wait i useEffect
   useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+    .then(res => {
+      return res.json()
+    })
+    .then((data) => {
+      //array of data:
+      console.log(data)
+      setBlogs(data);
+    })
     console.log("useEffect runs");
-   
   }, []);
-  //nu blir name en dependency!
+  
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} 
+      {blogs && <BlogList blogs={blogs} 
       title="Alla bloggar:" 
       handleDelete={handleDelete}>
-      </BlogList>
+      </BlogList> }
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Tova" )} 
       title="Tovas bloggar:"
       handleDelete={handleDelete}/> */}
