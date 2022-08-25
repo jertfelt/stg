f68cdb3ffab4cedd3880d3890f062801772def4d
Tova Jertfelt <tovizitan@gmail.com>
2022-08-25 09:29:02 +0200

    lagt till json

--- src/Home.js
+++ src/Home.js
@@ -2,25 +2,8 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList"
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    {title: "Blogga blogga med min kärlek", 
-    body:"Lorem ipsum...", 
-    author:"Tova", 
-    id: 4},
-    {title: "Kolla en blogg", 
-    body:"Lorem ipsum...", 
-    author:"Oskar", 
-    id: 1},
-    {title: "Kolla en till blogg", 
-    body:"Lorem ipsum...", 
-    author:"Jonas", 
-    id: 2},
-    {title: "Kolla min blogg dåråå", 
-    body:"Lorem ipsum...", 
-    author:"Tova", 
-    id: 3}
+    
   ])
-
-  const [name, setName] =useState("Daniel");
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id); 
@@ -29,9 +12,8 @@ const Home = () => {
 
   useEffect(() => {
     console.log("useEffect runs");
-    // console.log(blogs);
-    console.log(name)
-  }, [name]);
+   
+  }, []);
   //nu blir name en dependency!
 
   return ( 
@@ -40,11 +22,10 @@ const Home = () => {
       title="Alla bloggar:" 
       handleDelete={handleDelete}>
       </BlogList>
-      <BlogList blogs={blogs.filter((blog) => blog.author === "Tova" )} 
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Tova" )} 
       title="Tovas bloggar:"
-      handleDelete={handleDelete}/>
-      <button onClick={() => setName("Simon")}>Byt namn</button>
-      <p>{name}</p>
+      handleDelete={handleDelete}/> */}
+ 
     </div>
     );
 }
