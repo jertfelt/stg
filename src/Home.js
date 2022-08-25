b423cf4070def8ab99fd45056e7f305a13aa55d3
Tova Jertfelt <tovizitan@gmail.com>
2022-08-25 08:06:27 +0200

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,22 @@
 const Home = () => {
 
-  const handleClick = () => {
-    console.log("Yaaay you clicked")
+  const handleClick = (e) => {
+    console.log("Yaaay you clicked", e)
+  }
+
+  const handleClickAgain = (name, e) => 
+  {
+    console.log("HELLO " + name, e.target)
   }
 
   return ( 
    <div className="home">
     <h2>Homepage</h2>
     <button onClick={handleClick}>Click me! Click meeee!</button>
+    <button onClick={(e)=> 
+      handleClickAgain("Tova", e)
+    }>
+      Click me again</button>
    </div>
    );
 }
