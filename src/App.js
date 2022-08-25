219fecc2aa4aa2c9db3053ff7c9232e93a7542b4
Tova Jertfelt <tovizitan@gmail.com>
2022-08-25 07:35:03 +0200

    lektion 4 klar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
      <div classname="content">
-      <h1>App Component</h1>
+      <h1>{title}</h1>
+      <p> Liked {likes} times</p>
+      <p>{"Hello, world"}</p>
+      <p>{10}</p>
+      <p>{[1,2,3,4]}</p>
+      <p>{Math.random()*10}</p>
+      <a href= {link}>Länk </a>
      </div>
     </div>
   );
