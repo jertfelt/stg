a3203567dcade66f45726e603cc5e1310733d392
Tova Jertfelt <tovizitan@gmail.com>
2022-08-25 07:41:42 +0200

    lektion avslutad

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-
-import './App.css';
+import './App.css'; 
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "http://www.google.com";
+ 
 
   return (
     <div className="App">
+      <Navbar />
      <div classname="content">
-      <h1>{title}</h1>
-      <p> Liked {likes} times</p>
-      <p>{"Hello, world"}</p>
-      <p>{10}</p>
-      <p>{[1,2,3,4]}</p>
-      <p>{Math.random()*10}</p>
-      <a href= {link}>Länk </a>
+    <Home />
      </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (  
+    <nav className="navbar">
+      <h1>STG SPELAR SPEL</h1>
+      <div className="links">
+        <a href="/">Start</a>
+        <a href="/create">Ny blogg</a>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
+
