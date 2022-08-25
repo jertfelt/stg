c84e67d81ce153d443a7b1feb174d8c55878969e
Tova Jertfelt <tovizitan@gmail.com>
2022-08-25 09:42:54 +0200

    lagt in isLoading useState

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,9 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList"
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
 
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter(blog => blog.id !== id); 
-  //   setBlogs(newBlogs);
-  // }
-
-  //?går ej att använda async och wait i useEffect
+ 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
     .then(res => {
