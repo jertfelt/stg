a8c6b97f850c9b3372ea8b583e3bf22ccfe0295d
Tova Jertfelt <tovizitan@gmail.com>
2022-08-25 19:30:01 +0200

    method DELETE

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+
 
 const BlogDetails = () => {
   const {id} = useParams();
   const {data: blog, error, isLoading} = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
+
+  const handleDeleteButt = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE"
+    }).then(() => {
+      history.push("/");
+    })
+    
+  }
 
   return ( 
     <div className="blog-details">
@@ -14,6 +25,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p className="blog-author">{blog.author}</p>
           <div><p>{blog.body}</p></div>
+          <button onClick={handleDeleteButt}>Ta bort</button>
         </article>
       )}
     </div>
