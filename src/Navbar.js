e4b854e48516711780fb1345875f1a2df3a3fbf6
Tova Jertfelt <tovizitan@gmail.com>
2022-08-25 07:58:29 +0200

    css

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,12 @@ const Navbar = () => {
       <h1>STG SPELAR SPEL</h1>
       <div className="links">
         <a href="/">Start</a>
-        <a href="/create">Ny blogg</a>
+        <a href="/create" style={{
+          color: "black", 
+          backgroundColor: "cadetblue",
+          borderRadius: "8px"
+        }}>
+          Ny blogg</a>
       </div>
     </nav>
   );
